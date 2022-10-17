819878d71e5162bf56bccf91bc409e88c578fa02
Igor Rocha <igor_roc@hotmail.com.br>
2022-10-16 21:10:22 -0300

    [initial]

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
+import React from "react"
+import {
+	useFonts,
+	Roboto_300Light,
+	Roboto_400Regular,
+	Roboto_500Medium,
+	Roboto_700Bold,
+} from "@expo-google-fonts/roboto"
+import { ThemeProvider } from "styled-components/native"
 
 import THEME from "./src/theme"
 
+import { Home } from "./src/screens/Home"
 
 export default function App() {
+	const [fontsLoaded] = useFonts({
+		Roboto_300Light,
+		Roboto_400Regular,
+		Roboto_500Medium,
+		Roboto_700Bold,
+	})
+
+	if (!fontsLoaded) {
+		return null
+	}
+
 	return (
 		<ThemeProvider theme={THEME}>
 			<Home />
 		</ThemeProvider>
 	)
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
