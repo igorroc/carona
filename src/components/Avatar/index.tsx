a5feade88ff15f258c98422f3f8c6635b4013c1e
Igor Rocha <igor_roc@hotmail.com.br>
2022-10-17 07:04:06 -0300

    [new]: Avatar component

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,15 @@
+import blackWomanHappy from "../../assets/avatar/blackWomanHappy.png"
+import blackWomanNeutral from "../../assets/avatar/blackWomanNeutral.png"
+import whiteManBeard from "../../assets/avatar/whiteManBeard.png"
+import whiteManGlasses from "../../assets/avatar/whiteManGlasses.png"
+import whiteManHat from "../../assets/avatar/whiteManHat.png"
+import whiteWoman from "../../assets/avatar/whiteWoman.png"
+
+export const user = {
+	blackWomanHappy,
+	blackWomanNeutral,
+	whiteManBeard,
+	whiteManGlasses,
+	whiteManHat,
+	whiteWoman,
+}
--- src/components/Pill/index.tsx
+++ src/components/Pill/index.tsx
@@ -1,25 +1,9 @@
 import React from "react"
 
-import { Text, View, Image } from "react-native"
 import { LinearGradient } from "expo-linear-gradient"
 
 import { Avatar, Name, Wrapper } from "./styles"
-
-import blackWomanHappy from "../../assets/avatar/blackWomanHappy.png"
-import blackWomanNeutral from "../../assets/avatar/blackWomanNeutral.png"
-import whiteManBeard from "../../assets/avatar/whiteManBeard.png"
-import whiteManGlasses from "../../assets/avatar/whiteManGlasses.png"
-import whiteManHat from "../../assets/avatar/whiteManHat.png"
-import whiteWoman from "../../assets/avatar/whiteWoman.png"
-
-const user = {
-	blackWomanHappy,
-	blackWomanNeutral,
-	whiteManBeard,
-	whiteManGlasses,
-	whiteManHat,
-	whiteWoman,
-}
+import { user } from "../Avatar"
 
 interface PillProps {
 	name: string
