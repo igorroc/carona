881a20e9400086e0f6ea94c6d88ea9efc41f23af
Igor Rocha <igor_roc@hotmail.com.br>
2022-10-19 08:35:44 -0300

    [style]: Change DestaqueCard style

--- src/components/DestaqueCard/index.tsx
+++ src/components/DestaqueCard/index.tsx
@@ -1,9 +1,10 @@
 import { FontAwesome5 } from "@expo/vector-icons"
 import { LinearGradient } from "expo-linear-gradient"
 import React from "react"
+import { View } from "react-native"
 import { useTheme } from "styled-components"
 
-import { Description, Flex, Name, TopWrapper, Wrapper } from "./styles"
+import { Description, Flex, Name, IconWrapper, Wrapper } from "./styles"
 
 interface DestaqueProps {
 	name: string
@@ -17,39 +18,32 @@ export function DestaqueCard(props: DestaqueProps) {
 	return (
 		<Flex>
 			<LinearGradient
-				colors={[theme.COLORS.BLACK3, theme.COLORS.BLACK2]}
-				end={{ x: 0, y: 1 }}
-				style={{
-					width: 100,
-					height: 40,
-					borderTopLeftRadius: 10,
-					borderTopRightRadius: 10,
-				}}
-			>
-				<TopWrapper>
-					<FontAwesome5
-						name={props.icon}
-						size={24}
-						color={theme.COLORS.PRIMARY}
-						style={{
-							marginLeft: 20,
-						}}
-					/>
-				</TopWrapper>
-			</LinearGradient>
-			<LinearGradient
-				colors={[theme.COLORS.BLACK2, theme.COLORS.BLACK3]}
+				colors={["#36473D30", "#22342A30"]}
 				end={{ x: 0, y: 1 }}
 				style={{
 					// flex: 1,
 					// height: 100,
-					borderTopLeftRadius: 0,
-					borderTopRightRadius: 10,
-					borderBottomLeftRadius: 10,
-					borderBottomRightRadius: 10,
+					borderRadius: 10,
+					borderWidth: 1,
+					borderColor: "#fff3",
 				}}
 			>
 				<Wrapper>
+					<IconWrapper
+					>
+						<FontAwesome5
+							name={props.icon}
+							size={20}
+							color={theme.COLORS.PRIMARY}
+							style={{
+								width: 20,
+								height: 20,
+								alignItens: "center",
+								justifyContent: "center",
+							}}
+						/>
+					</IconWrapper>
+
 					<Name>{props.name}</Name>
 					<Description>{props.description}</Description>
 				</Wrapper>
--- src/components/DestaqueCard/styles.ts
+++ src/components/DestaqueCard/styles.ts
@@ -4,21 +4,27 @@ export const Flex = styled.View`
 	margin: 0 10px;
 `
 
-export const TopWrapper = styled.View`
-	height: 100%;
+export const IconWrapper = styled.View`
+	align-self: flex-start;
 	justify-content: center;
-	/* align-items: center; */
+	align-items: center;
+	background-color: #fff3;
+	padding: 20px;
+	border-radius: 10px;
 `
 
 export const Wrapper = styled.View`
 	margin: 20px;
 	justify-content: center;
+	gap: 100px;
+	min-width: 140px;
 `
 
 export const Name = styled.Text`
 	font-family: ${({ theme }) => theme.FONTS.SEMIBOLD};
 	font-size: ${({ theme }) => theme.FONTSIZES.H8}px;
 	color: ${({ theme }) => theme.COLORS.WHITE};
+	margin: 10px 0;
 `
 
 export const Description = styled.Text`
