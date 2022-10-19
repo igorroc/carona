45725b05e8ff3c2416cbdf9886d1aa48bfbb3d9e
Igor Rocha <igor_roc@hotmail.com.br>
2022-10-19 18:23:31 -0300

    [add]: New Avatars

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -4,6 +4,9 @@ import whiteManBeard from "../../assets/avatar/whiteManBeard.png"
 import whiteManGlasses from "../../assets/avatar/whiteManGlasses.png"
 import whiteManHat from "../../assets/avatar/whiteManHat.png"
 import whiteWoman from "../../assets/avatar/whiteWoman.png"
+import blackManHappy from "../../assets/avatar/blackManHappy.png"
+import whiteWomanColorful from "../../assets/avatar/whiteWomanColorful.png"
+import man from "../../assets/avatar/man.png"
 import link from "../../assets/icon/link.png"
 
 export const user = {
@@ -13,5 +16,8 @@ export const user = {
 	whiteManGlasses,
 	whiteManHat,
 	whiteWoman,
+	blackManHappy,
+	whiteWomanColorful,
 	link,
+	man,
 }
