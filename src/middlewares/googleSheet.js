f96c42319e644df1a04aa28cdf3168f66acfdbf0
Igor Rocha <igor_roc@hotmail.com.br>
2022-10-19 22:43:37 -0300

    [WIP]: Get avatars

--- src/middlewares/googleSheet.js
+++ src/middlewares/googleSheet.js
@@ -1,13 +1,29 @@
 const DataAPI = async (page) => {
-	const sheetURL = `https://sheets.googleapis.com/v4/spreadsheets/${GOOGLE_SPREADSHEET_ID}/values/${page}?valueRenderOption=FORMATTED_VALUE&key=${GOOGLE_SPREADSHEET_API_KEY}`
+	const sheetURL = `https://sheets.googleapis.com/v4/spreadsheets/${GOOGLE_SPREADSHEET_ID}/values/${page}?valueRenderOption=UNFORMATTED_VALUE&key=${GOOGLE_SPREADSHEET_API_KEY}`
 
 	try {
 		let data = await fetch(sheetURL)
 		let { values } = await data.json()
 		let [, ...Data] = await values.map((data) => data)
 		return Data
-	} catch {
-		console.log("Error")
+	} catch (err) {
+		console.log("Error", err.message)
+		console.log(err)
 	}
 }
+
+const getAvatars = async () => {
+	let avatars = {}
+
+	await DataAPI("Personagens").then((response) => {
+		console.log(response)
+		data.forEach((item) => {
+			console.log(item)
+			avatars[item[0]] = item[1] || "man"
+		})
+	})
+
+	return avatars
+}
+
 export default DataAPI
